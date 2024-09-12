fb615dd1bdd104daf88b2439b481e5d4be014f03
IT21221064 <it21221064@my.sliit.lk>
2024-09-12 23:54:45 +0530

    fix: Cross-site Scripting fixed

--- backend/routes/departmentRoute.js
+++ backend/routes/departmentRoute.js
@@ -1,116 +1,118 @@
-import express from 'express';
+import express from "express";
 import { Department } from "../models/departmentModel.js";
 
 const router = express.Router();
 
 // Route for save a new department
-router.post('/', async (request, response) => {
-    try{
-        if(
-            !request.body.departmentName ||
-            !request.body.departmentDetails ||
-            !request.body.createdOn ||
-            !request.body.departmentStatus 
-
-        ) {
-            return response.status(400).send({
-               message: 'Send all reuired fields:departmentName, departmentDetails, createdOn, departmentStatus',
-            });
-        }
-        const newDepartment = {
-            departmentName: request.body.departmentName,
-            departmentDetails: request.body.departmentDetails,
-            createdOn: request.body.createdOn,
-            departmentStatus: request.body.departmentStatus,
-        };
-
-        const department = await Department.create(newDepartment);
-
-        return response.status(201).send(department);
-
-    }catch (error) {
-        console.log(error.message); 
-        response.status(500).send({ message: error.message });
+router.post("/", async (request, response) => {
+  try {
+    if (
+      !request.body.departmentName ||
+      !request.body.departmentDetails ||
+      !request.body.createdOn ||
+      !request.body.departmentStatus
+    ) {
+      return response.status(400).send({
+        message:
+          "Send all reuired fields:departmentName, departmentDetails, createdOn, departmentStatus",
+      });
     }
+    const newDepartment = {
+      departmentName: request.body.departmentName,
+      departmentDetails: request.body.departmentDetails,
+      createdOn: request.body.createdOn,
+      departmentStatus: request.body.departmentStatus,
+    };
+
+    const department = await Department.create(newDepartment);
+
+    return response.status(201).send(department);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 // Route for Get All Departments from database
-router.get('/', async (request, response) => {
-    try{
-        const departments = await Department.find({});
+router.get("/", async (request, response) => {
+  try {
+    const departments = await Department.find({});
 
-        return response.status(200).json({
-           count:  departments.length,
-           data: departments
-        });
-
-    }catch (error) {
-        console.log(error.message); 
-        response.status(500).send({ message: error.message });
-    }
+    return response.status(200).json({
+      count: departments.length,
+      data: departments,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 // Route for Get One Departments from database by id
-router.get('/:id', async (request, response) => {
-    try{
-        const { id } = request.params;
+router.get("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
 
-        const department = await Department.findById(id);
+    const department = await Department.findById(id);
 
-        return response.status(200).json(department);
-
-    }catch (error) {
-        console.log(error.message); 
-        response.status(500).send({ message: error.message });
-    }
+    return response.status(200).json(department);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 // Route for updating a department
-router.put('/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
+router.put("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
 
-        // Find department by ID
-        const department = await Department.findById(id);
+    // Find department by ID
+    const department = await Department.findById(id);
 
-        if (!department) {
-            return response.status(404).json({ message: 'Department not found' });
-        }
-
-       
-        department.departmentName = 'Updated Department Name';
-        department.departmentDetails = 'Updated Department Details';
-        department.createdOn = new Date(); // or provide specific date
-        department.departmentStatus = 'Updated Status';
-
-        // Save updated department
-        await department.save();
-
-        return response.status(200).json({ message: 'Department details updated successfully', updatedDepartment: department });
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: 'Error updating department details' });
+    if (!department) {
+      return response.status(404).json({ message: "Department not found" });
     }
+
+    department.departmentName = "Updated Department Name";
+    department.departmentDetails = "Updated Department Details";
+    department.createdOn = new Date(); // or provide specific date
+    department.departmentStatus = "Updated Status";
+
+    // Save updated department
+    await department.save();
+
+    return response
+      .status(200)
+      .json({
+        message: "Department details updated successfully",
+        updatedDepartment: department,
+      });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: "Error updating department details" });
+  }
 });
 
 //Route for Delete a Department
-router.delete('/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
+router.delete("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
 
-        // Find department by ID and delete
-        const result = await Department.findByIdAndDelete(id);
+    // Find department by ID and delete
+    const result = await Department.findByIdAndDelete(id);
 
-        if (!result) {
-            return response.status(404).json({ message: 'Department not found' });
-        }
-
-        return response.status(200).json({ message: 'Department deleted successfully' });
-    } catch (error) {
-        console.log(error.message); 
-        response.status(500).send({ message: error.message });
+    if (!result) {
+      return response.status(404).json({ message: "Department not found" });
     }
-});
 
+    return response
+      .status(200)
+      .json({ message: "Department deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 export default router;
--- backend/routes/employeeRoute.js
+++ backend/routes/employeeRoute.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import path from 'path';
-import multer from 'multer';
-import nodemailer from 'nodemailer';
-import { Employee } from '../models/employeeModel.js';
-import { generatePassword } from '../utils/passwordUtils.js';
-import bcrypt from 'bcrypt';
-
+import express from "express";
+import path from "path";
+import multer from "multer";
+import nodemailer from "nodemailer";
+import { Employee } from "../models/employeeModel.js";
+import { generatePassword } from "../utils/passwordUtils.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 const __dirname = path.resolve();
@@ -13,26 +12,26 @@ const __dirname = path.resolve();
 // Multer configuration for file upload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads'); // Specify the directory where uploaded files should be stored
+    cb(null, "uploads"); // Specify the directory where uploaded files should be stored
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); // Generate unique file names
-  }
+    cb(null, Date.now() + "-" + file.originalname); // Generate unique file names
+  },
 });
 
 const upload = multer({ storage: storage });
 
 // Serve static files from the 'uploads' directory
-router.use('/uploads', express.static('uploads'));
+router.use("/uploads", express.static("uploads"));
 
 // Function to generate a unique employee ID
 function generateEmployeeId() {
   // Prefix for employee ID
-  const prefix = 'EMI';
-  
+  const prefix = "EMI";
+
   // Generate a random number (you can replace this with a more sophisticated logic)
   const randomNumber = Math.floor(Math.random() * 1000); // Generates a random number between 0 and 999
-  
+
   // Combine prefix and random number to create the employee ID
   const employeeId = prefix + randomNumber;
 
@@ -40,32 +39,34 @@ function generateEmployeeId() {
 }
 
 // Route for employee login
-router.post('/login', async (request, response) => {
+router.post("/login", async (request, response) => {
   try {
     const { employeeEmail, password } = request.body;
 
     const employee = await Employee.findOne({ employeeEmail });
 
     if (!employee) {
-      return response.status(404).json({ message: 'Employee not found' });
+      return response.status(404).json({ message: "Employee not found" });
     }
 
     // Ensure that the password is fetched properly from the database
-    console.log('Comparing password:', password);
-    console.log('Hashed password:', employee.password);
+    // console.log('Comparing password:', password);
+    // console.log('Hashed password:', employee.password);
 
     // Compare provided password with hashed password
     const isPasswordValid = await bcrypt.compare(password, employee.password);
 
     if (!isPasswordValid) {
-      return response.status(401).json({ message: 'Invalid email or password' });
+      return response
+        .status(401)
+        .json({ message: "Invalid email or password" });
     }
 
     // If credentials are valid, you can generate a token or simply send a success message
-    return response.status(200).json({ message: 'Login successful' });
+    return response.status(200).json({ message: "Login successful" });
   } catch (error) {
-    console.error('Error during login:', error);
-    return response.status(500).json({ message: 'Internal server error' });
+    console.error("Error during login:", error);
+    return response.status(500).json({ message: "Internal server error" });
   }
 });
 // Function to hash the password
@@ -75,16 +76,32 @@ const hashPassword = async (password) => {
 };
 
 // Route for adding a new employee with image upload
-router.post('/', upload.single('image'), async (request, response) => {
+router.post("/", upload.single("image"), async (request, response) => {
   try {
-    const { employeeName, employeeEmail, employeeMobile, employeeAddress, employeeRole, createdOn, password } = request.body;
+    const {
+      employeeName,
+      employeeEmail,
+      employeeMobile,
+      employeeAddress,
+      employeeRole,
+      createdOn,
+      password,
+    } = request.body;
 
-    if (!employeeName || !employeeEmail || !employeeMobile || !employeeAddress || !employeeRole || !createdOn || !password) {
+    if (
+      !employeeName ||
+      !employeeEmail ||
+      !employeeMobile ||
+      !employeeAddress ||
+      !employeeRole ||
+      !createdOn ||
+      !password
+    ) {
       return response.status(400).json({
-        message: 'Send all required fields: employeeName, employeeEmail, employeeMobile, employeeAddress, employeeRole, createdOn, password',
+        message:
+          "Send all required fields: employeeName, employeeEmail, employeeMobile, employeeAddress, employeeRole, createdOn, password",
       });
     }
-    
 
     // Generate a unique employee ID
     const employeeId = generateEmployeeId();
@@ -101,46 +118,50 @@ router.post('/', upload.single('image'), async (request, response) => {
       employeeRoles: employeeRole,
       createdOn,
       password: hashedPassword,
-      image: request.file ? path.join('uploads', request.file.filename) : null,
+      image: request.file ? path.join("uploads", request.file.filename) : null,
     });
 
     await newEmployee.save();
 
-    return response.status(201).json({ message: 'Employee added successfully' });
+    return response
+      .status(201)
+      .json({ message: "Employee added successfully" });
   } catch (error) {
-    console.error('ServerError:', error);
-    return response.status(500).json({ message: 'An error occurred on the server' });
+    console.error("ServerError:", error);
+    return response
+      .status(500)
+      .json({ message: "An error occurred on the server" });
   }
 });
 
 // Route for getting all employees
-router.get('/', async (request, response) => {
+router.get("/", async (request, response) => {
   try {
     const employees = await Employee.find({});
     response.status(200).json({ count: employees.length, data: employees });
   } catch (error) {
     console.error(error);
-    response.status(500).json({ message: 'An error occurred on the server' });
+    response.status(500).json({ message: "An error occurred on the server" });
   }
 });
 
 // Route for getting a specific employee by ID
-router.get('/:id', async (request, response) => {
+router.get("/:id", async (request, response) => {
   try {
     const { id } = request.params;
     const employee = await Employee.findById(id);
     if (!employee) {
-      return response.status(404).json({ message: 'Employee not found' });
+      return response.status(404).json({ message: "Employee not found" });
     }
     response.status(200).json(employee);
   } catch (error) {
     console.error(error);
-    response.status(500).json({ message: 'An error occurred on the server' });
+    response.status(500).json({ message: "An error occurred on the server" });
   }
 });
 
 // Route for updating an employee
-router.put('/:id', async (request, response) => {
+router.put("/:id", async (request, response) => {
   try {
     const { id } = request.params;
     const updatedEmployee = {
@@ -151,48 +172,54 @@ router.put('/:id', async (request, response) => {
       employeeRoles: request.body.employeeRoles,
       createdOn: request.body.createdOn,
     };
-    const employee = await Employee.findByIdAndUpdate(id, updatedEmployee, { new: true });
+    const employee = await Employee.findByIdAndUpdate(id, updatedEmployee, {
+      new: true,
+    });
     if (!employee) {
-      return response.status(404).json({ message: 'Employee not found' });
+      return response.status(404).json({ message: "Employee not found" });
     }
-    response.status(200).json({ message: 'Employee details updated successfully' });
+    response
+      .status(200)
+      .json({ message: "Employee details updated successfully" });
   } catch (error) {
     console.error(error);
-    response.status(500).json({ message: 'An error occurred on the server' });
+    response.status(500).json({ message: "An error occurred on the server" });
   }
 });
 
 // Route for deleting an employee
-router.delete('/:id', async (request, response) => {
+router.delete("/:id", async (request, response) => {
   try {
     const { id } = request.params;
     const deletedEmployee = await Employee.findByIdAndDelete(id);
     if (!deletedEmployee) {
-      return response.status(404).json({ message: 'Employee not found' });
+      return response.status(404).json({ message: "Employee not found" });
     }
-    response.status(200).json({ message: 'Employee profile deleted successfully' });
+    response
+      .status(200)
+      .json({ message: "Employee profile deleted successfully" });
   } catch (error) {
     console.error(error);
-    response.status(500).json({ message: 'An error occurred on the server' });
+    response.status(500).json({ message: "An error occurred on the server" });
   }
 });
 
 // Define your nodemailer transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    user: '2001imashaperera@gmail.com',
-    pass: 'dxqqgfymqnymblwf',
+    user: "2001imashaperera@gmail.com",
+    pass: "dxqqgfymqnymblwf",
   },
 });
 
 // Define your API endpoint to send emails
-router.post('/send_email', async (req, res) => {
+router.post("/send_email", async (req, res) => {
   const { employeeEmail } = req.body;
   try {
     // Generate a random password
     const password = generatePassword();
-    
+
     const url = `http://localhost:5173/HomePage`;
 
     const emailContent = `
@@ -207,21 +234,18 @@ router.post('/send_email', async (req, res) => {
     await transporter.sendMail({
       from: {
         name: "Employee-Manage Department",
-        address: '2001imashaperera@gmail.com'
+        address: "2001imashaperera@gmail.com",
       },
       to: employeeEmail,
       subject: "Invitation to Join Us",
-      html: emailContent
+      html: emailContent,
     });
 
-    res.status(200).json({ message: 'Email sent successfully' });
+    res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error sending email:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
-
-
-
 
 export default router;
